c4ec9bfba6dc89ffd3d6d7281a37eeaa0f0b0019
Casey Sayre <casey-sayre@users.noreply.github.com>
2023-03-21 19:06:19 +0000

    defer in-line editing of price

--- src/AlbumRow.tsx
+++ src/AlbumRow.tsx
@@ -1,5 +1,5 @@
 import { Grid, Paper, Stack, TextField, Typography } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { IAlbum } from "./Album";
 
 type Props = {
@@ -8,12 +8,6 @@ type Props = {
 }
 
 export const AlbumRow = ({ album, onPriceEdit }: Props) => {
-
-  const priceRef = useRef("");
-
-  useEffect(() => {
-   priceRef.current = album.price.toString()
-  }, []);
 
   return <Grid container>
     <Grid item xs={4}>
@@ -31,16 +25,11 @@ export const AlbumRow = ({ album, onPriceEdit }: Props) => {
       </Paper>
     </Grid>
     <Grid item xs={3}>
-      <TextField margin="none" variant="outlined" color="primary"
-        value={priceRef.current}
-        onChange={e => {
-          priceRef.current = e.target.value;
-          onPriceEdit({
-            ...album,
-            price: Number(e.target.value)
-          })
-        }}
-      />
+      <Paper component={Stack} direction="column" justifyContent="right" height={"100%"} elevation={0} sx={{ backgroundColor: 'transparent' }}>
+        <Typography variant="body1" color="primary">
+          {album.price}
+        </Typography>
+      </Paper>
     </Grid>
   </Grid>
 
